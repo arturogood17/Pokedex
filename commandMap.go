2fac3494c932cb23d8128143a296670d4b452a5a
arturogood17 <arturo_good17@hotmail.com>
2025-03-02 18:23:13 +0100

    Location list applied to both commands

--- commandMap.go
+++ commandMap.go
@@ -1,43 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 )
 
 func MapCommand(c *config) error {
-	var url string
-	if c.nextURL == "" {
-		url = baseURL + "location-area/"
-	} else {
-		url = c.nextURL
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
+	locationList, err := c.pokeClient.ListLocation(c.nextURL)
 	if err != nil {
 		return err
 	}
-
-	res, err := c.Clnt.HttpClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-	var locationList LocationArea
-	json.Unmarshal(data, &locationList)
-	c.previousURL = locationList.Previous
-	c.nextURL = locationList.Next
 
 	for _, loc := range locationList.Results {
 		fmt.Println(loc.Name)
 	}
+	c.nextURL = locationList.Next
+	c.previousURL = locationList.Previous
+
 	return nil
 }
 
@@ -45,23 +24,10 @@ func MapbCommand(c *config) error {
 	if c.previousURL == nil {
 		return errors.New("you're on the first page")
 	}
-	url := c.previousURL
-	req, err := http.NewRequest("GET", *url, nil)
+	locationList, err := c.pokeClient.ListLocation(c.previousURL)
 	if err != nil {
 		return err
 	}
-
-	res, err := c.Clnt.HttpClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-	var locationList LocationArea
-	json.Unmarshal(data, &locationList)
 	c.previousURL = locationList.Previous
 	c.nextURL = locationList.Next
 
