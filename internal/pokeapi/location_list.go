453214e7db6797981de876e48da5e807194dac35
arturogood17 <arturo_good17@hotmail.com>
2025-03-02 18:22:32 +0100

    DRY for location list request

---
+++ internal/pokeapi/location_list.go
@@ -0,0 +1,34 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocation(pageURL *string) (LocationArea, error) {
+	url := baseURL + "location-area"
+	if pageURL != nil {
+		url = *pageURL
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	res, err := c.HttpClient.Do(req)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	var locationList LocationArea
+	if err := json.Unmarshal(data, &locationList); err != nil {
+		return LocationArea{}, err
+	}
+	return locationList, nil
+}
