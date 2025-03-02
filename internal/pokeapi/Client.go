34e7db3282e4c86813a86bb1c29f78636a1bb727
arturogood17 <arturo_good17@hotmail.com>
2025-03-02 18:21:57 +0100

    http.Client already has a timeout parameter

--- internal/pokeapi/Client.go
+++ internal/pokeapi/Client.go
@@ -9,8 +9,8 @@ type Client struct { // http.Client ya tiene timeout
 	HttpClient http.Client
 }
 
-func NewClient(timeout time.Duration) *Client {
-	c := &Client{
+func NewClient(timeout time.Duration) Client {
+	c := Client{
 		HttpClient: http.Client{
 			Timeout: timeout,
 		},
