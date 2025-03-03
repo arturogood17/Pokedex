bc40cf38c4b0e99207e707a5c5bcfee54bb63c5f
arturogood17 <arturo_good17@hotmail.com>
2025-03-03 22:52:03 +0100

    Client now uses cache

--- internal/pokeapi/Client.go
+++ internal/pokeapi/Client.go
@@ -3,17 +3,21 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/arturogood17/pokedex/internal/pokecache"
 )
 
 type Client struct { // http.Client ya tiene timeout
 	HttpClient http.Client
+	cache      pokecache.Cache
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, interval time.Duration) Client {
 	c := Client{
 		HttpClient: http.Client{
 			Timeout: timeout,
 		},
+		cache: pokecache.NewCache(interval),
 	}
 	return c
 }
