f70950ceb4b7fa0baf05b5de408fcf381cf77e96
arturogood17 <arturo_good17@hotmail.com>
2025-03-03 22:52:30 +0100

    Create a client with a cache field

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func cleanInput(text string) []string {
 
 func main() {
 
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
 	cfg := &config{
 		pokeClient: pokeClient,
 	}
