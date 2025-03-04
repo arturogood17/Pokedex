8916d201e00e8e046962ae8d3f06e8cc714dceb4
arturogood17 <arturo_good17@hotmail.com>
2025-03-04 19:01:35 +0100

    Explore command

--- commandExplore.go
+++ commandExplore.go
@@ -1,5 +1,24 @@
 package main
 
-func ExploreCommand(cfg *config, areas ...string) error {
+import (
+	"errors"
+	"fmt"
+)
 
+func ExploreCommand(cfg *config, areas ...string) error {
+	if len(areas) == 0 {
+		return errors.New("area needed")
+	}
+	area := areas[0]
+
+	area_poke, err := cfg.pokeClient.AreaPokemon(&area)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Exploring %s...\n", area)
+	fmt.Println("Found Pokemon:")
+	for _, val := range area_poke.PokemonEncounters {
+		fmt.Printf("- %s\n", val.Pokemon.Name)
+	}
+	return nil
 }
