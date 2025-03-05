520286d128b013cc1959a8382e25166ed3b72d0f
arturogood17 <arturo_good17@hotmail.com>
2025-03-06 00:54:05 +0100

    Changes in logic to test and minor fixes

--- commandCatch.go
+++ commandCatch.go
@@ -9,7 +9,7 @@ import (
 )
 
 func CatchCommand(c *config, cmds ...string) error {
-	if len(cmds) != 0 {
+	if len(cmds) == 0 {
 		return errors.New("provide a pokemon name")
 	}
 	name := cmds[0]
@@ -19,12 +19,15 @@ func CatchCommand(c *config, cmds ...string) error {
 		return err
 	}
 	var caught = make(map[string]pokeapi.Pokemon)
-	bingo := 5 + 1
-	catchingProb := rand.Intn(bingo)
+	bingo := 3
+	catchingProb := rand.Intn(bingo) + 1
 	fmt.Printf("Throwing a Pokeball at %s\n", pokemon.Name)
 	if catchingProb == bingo {
 		fmt.Printf("%s was caught\n", pokemon.Name)
-		caught[pokemon.Name] = pokemon
+		caught[pokemon.Name] = pokemon //hay que crear un pokedex.go que se pueda llamar para guardar los pokemones atrapados
+	} else {
+		fmt.Printf("%d\n", catchingProb)
+		fmt.Printf("%s escaped\n", pokemon.Name)
 	}
 	return nil
 }
